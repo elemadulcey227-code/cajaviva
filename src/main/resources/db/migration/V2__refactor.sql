08502cce51981a8fe42b3def0c365a1ef2bff981
Emmanuel <elemadulcey227@gmail.com>
2026-03-11 14:57:29 -0500

    Correcciones de singularidad y FK en V2

--- src/main/resources/db/migration/V2__refactor.sql
+++ src/main/resources/db/migration/V2__refactor.sql
@@ -1,10 +1,22 @@
--- Agregar constraint de fechas en RecurrentTransaction
+-- Email único en Person
+ALTER TABLE Person
+ADD CONSTRAINT UQ_Person_Email UNIQUE(email);
+
+-- Balance no negativo en Account
+ALTER TABLE Account
+ADD CONSTRAINT chk_balance CHECK (balance >= 0);
+
+-- Valor no negativo en RecurrentTransaction
+ALTER TABLE RecurrentTransaction
+ADD CONSTRAINT chk_value CHECK (value >= 0);
+
+-- Constraint de fechas en RecurrentTransaction
 ALTER TABLE RecurrentTransaction
 ADD CONSTRAINT chk_dates CHECK (end_date IS NULL OR end_date >= initial_date);
 
--- Asegurar que email sea único en Person
-ALTER TABLE Person
-ADD CONSTRAINT UQ_Person_Email UNIQUE(email);
+-- Agregar columna category_id en RecurrentTransaction
+ALTER TABLE RecurrentTransaction
+ADD category_id UNIQUEIDENTIFIER;
 
 -- Agregar FK de category_id en RecurrentTransaction
 ALTER TABLE RecurrentTransaction
